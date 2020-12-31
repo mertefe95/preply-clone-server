0a57d45301de0d7af7727fd00f6644de81e6672b
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-12-31 19:36:42 +0300

    routes created

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const studentRoute = require("./routes/student");
+const tutorRoute = require("./routes/tutor");
+require("dotenv").config();
 
+console.log(process.env.ATLAS_URI);
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/students", studentRoute);
+app.use("/tutors", tutorRoute);
 
+const { connection } = mongoose;
 
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-  console.log("MongoDB connection has been established.")
+connection.once("open", () => {
+  console.log("MongoDB connection has been established.");
 });
 
 const uri = process.env.ATLAS_URI;
 
-
 mongoose.connect(uri, {
   useCreateIndex: true,
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
+  useUnifiedTopology: true,
+});
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`${PORT} is running on.`)
-})
+  console.log(`${PORT} is running on.`);
+});
